6a9cc20ce8999f738c6baa39daaecebae1013382
Kris <kris_mb@abv.bg>
2019-08-23 18:05:34 +0300

    Adding the tf remote state base for the solution

---
+++ env/base/main.tf
@@ -0,0 +1,15 @@
+provider aws { }
+
+module "terraform-backend" {
+  source = "github.com/ElAntagonista/tf-rifiniti-modules/common/terraform-aws-rs"
+  s3_bucket_name = "tfrifiniti"
+  dynamodb_table_name = "tfrifiniti"
+}
+
+output "s3_bucket_domain_name" {
+  value = "${module.terraform-backend.s3_bucket_domain_name}"
+}
+
+output "s3_bucket_regional_domain_name" {
+  value = "${module.terraform-backend.s3_bucket_regional_domain_name}"
+}
